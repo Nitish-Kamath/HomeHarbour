f7952c559ff42cb18765aaeae43914bfb6e33d7b
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-03 22:39:37 +0530

    create sign up route api

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 // express is used for creating backend server
@@ -20,6 +20,9 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => console.log("server is running on port number 3000"));
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
