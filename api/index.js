8fe4b9358e1abc7fcebe6338a33bedd3a03a3c5d
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-03 15:44:27 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+// express is used for creating
+
+// backend server
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000, () => console.log("server is running on port number 4000"));
+app.listen(3000, () => console.log("server is running on port number 3000"));
