3358f61c2b75e88c1cebe30d9855b204ea54e168
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-06 18:08:53 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
+
+// secure: true means:
+
+// A backend server running on HTTPS with an invalid certificate will not be accepted by default.
+
+// So if you want to accept invalid (missconfigured, broken cert chain, revoked, etc. ) cert than use secure: false
