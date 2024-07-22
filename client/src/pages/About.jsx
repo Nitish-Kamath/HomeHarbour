f438f3de58bb0c7c70af4939f9e602c012fced78
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-23 02:17:32 +0530

    add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,14 +4,13 @@ export default function About() {
   return (
     <div className=" py-20 px-4 max-w-6xl mx-auto ">
       <h1 className=" text-3xl font-bold mb-4 text-slate-800 ">
-        About Nitish Estate
+        About HomeHarbour
       </h1>
       <p className=" mb-4 text-slate-700  ">
-        Nitish Estate is a leading real estate agency that specializes in
-        helping client buy, and rent properties in the most desirable
-        neighbourhoods. Our team of experienced agents is dedicated in providing
-        exceptional service and making the buying and selling as smooth as
-        possible.
+        HomeHarbour is a leading real estate agency that specializes in helping
+        client buy, and rent properties in the most desirable neighbourhoods.
+        Our team of experienced agents is dedicated in providing exceptional
+        service and making the buying and selling as smooth as possible.
       </p>
       <p className=" mb-4 text-slate-700  ">
         Our mission is to help our clients achiver their real estate goals by
